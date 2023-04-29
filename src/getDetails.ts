8790069da39458bc728f29ca09af1ef6efcb4632
Anurag30112003 <anuragsharma011011@gmail.com>
2023-04-29 22:50:10 +0530

    added

---
+++ src/getDetails.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { openAi } from "./config/openAi.config";
+
+export default async function getDetails(text:string,language:string) {
+  const response = await openAi.createCompletion({
+    model: "text-davinci-003",
+    prompt:
+      `You are an AI which explains what is wrong with his code , generally i'll provide the lanuage also .\nand also suggest how to resolve it.\n\nhere is example response\n\ncode - \nlanguage - typescript\n\nconst hello:number = 'main';\n\nconsole.log(hello);\n\nresponse - \nThis code is incorrect because the variable hello is declared as a number data type, but it is assigned a string value. To fix this, you should change the data type of the variable hello to a string, such as: const hello:string = 'main'.\n\n\ncode - ${text} \n language - ${language}\n\nresponse -  `,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return response.data.choices[0].text;
+
+}
